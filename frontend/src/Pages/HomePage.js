38972eb374f99e3a4fbd3f17933ef6eb1dc84892
Manish Sharma <manishsharma@192.168.1.12>
2025-12-06 20:43:53 +0530

    plan

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -10,33 +10,45 @@ import { useNavigate } from 'react-router-dom'
 // static sample plans (rendered on home page)
 const SAMPLE_PLANS = [
     {
-        _id: '69147dcc649c109a2a85299c',
-        title: 'Annually',
-        price: 4.99,
-        subtitle: 'only digital card',
-        duration: 365,
+        "_id": "6914d180e1514234b360e8db",
+        "title": "Annually",
+        "price": 399,
+        "subtitle": "only digital card",
+        "duration": 365,
+        "createdAt": "2025-11-12T18:27:12.867Z",
+        "updatedAt": "2025-12-06T13:40:19.157Z",
+        "__v": 0
     },
     {
-        _id: '69148153649c109a2a8529a6',
-        title: 'Annually',
-        price: 9.99,
-        subtitle: 'digital + plastic NFC card',
-        duration: 365,
+        "_id": "6914d18ee1514234b360e8de",
+        "title": "Annually",
+        "price": 749,
+        "subtitle": "digital + plastic card",
+        "duration": 365,
+        "createdAt": "2025-11-12T18:27:26.135Z",
+        "updatedAt": "2025-12-06T13:40:33.711Z",
+        "__v": 0
     },
     {
-        _id: '6914816a649c109a2a8529a9',
-        title: 'Annually',
-        price: 24.99,
-        subtitle: 'digital + metal card',
-        duration: 365,
+        "_id": "6914d198e1514234b360e8e1",
+        "title": "Annually",
+        "price": 1599,
+        "subtitle": "digital + metal card",
+        "duration": 365,
+        "createdAt": "2025-11-12T18:27:36.436Z",
+        "updatedAt": "2025-12-06T13:40:47.532Z",
+        "__v": 0
     },
     {
-        _id: '6914819d649c109a2a8529ad',
-        title: 'Annually',
-        price: 99,
-        subtitle: 'digital + plastic card',
-        duration: 365,
-    },
+        "_id": "6914d1a2e1514234b360e8e4",
+        "title": "Annually",
+        "price": 9000,
+        "subtitle": "digital + NFC card",
+        "duration": 365,
+        "createdAt": "2025-11-12T18:27:46.189Z",
+        "updatedAt": "2025-12-06T13:41:00.955Z",
+        "__v": 0
+    }
 ]
 
 const HomePage = () => {
